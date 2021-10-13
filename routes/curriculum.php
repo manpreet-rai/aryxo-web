ef262968d5df7501830ba3b455cf13dad0a4d01b
aryxo-in <aryxo21@gmail.com>
2021-10-13 20:12:57 +0530

    updated

--- routes/curriculum.php
+++ routes/curriculum.php
@@ -16,6 +16,11 @@ Route::get('c', function () {
     return view('curriculum', ['course' => 'c', 'titles' => $titles, 'seo' => 'seo.c']);
 });
 
+Route::get('java', function () {
+    $titles = json_decode(file_get_contents(resource_path('views/curriculums/c.json')), true);
+    return view('curriculum', ['course' => 'java', 'titles' => $titles, 'seo' => 'seo.c']);
+});
+
 Route::get('python', function () {
     $titles = json_decode(file_get_contents(resource_path('views/curriculums/python.json')), true);
     return view('curriculum', ['course' => 'python', 'titles' => $titles, 'seo' => 'seo.python']);
