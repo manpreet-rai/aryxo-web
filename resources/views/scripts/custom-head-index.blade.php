0c934830836fa7555f192b8f31afac2eebaad0f8
Aryxo <91327602+aryxo-in@users.noreply.github.com>
2021-11-02 15:19:24 +0530

    Updared Screen Size

--- resources/views/scripts/custom-head-index.blade.php
+++ resources/views/scripts/custom-head-index.blade.php
@@ -1,6 +1,13 @@
 <script>
     let localstorage = window.localStorage;
     window.onload = function () {
+        let platform = window.navigator.platform,
+            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+        
+        if (windowsPlatforms.indexOf(platform) !== -1){
+            document.body.classList.toggle('scale-80');
+        }
+        
         let cookieConsent = document.getElementById('cookie-consent');
         if (localstorage.getItem('cookies-access') !== 'true') {
             let footer = document.getElementsByTagName('footer');
--- resources/views/scripts/custom-head.blade.php
+++ resources/views/scripts/custom-head.blade.php
@@ -1,6 +1,13 @@
 <script>
     let localstorage = window.localStorage;
     window.onload = function () {
+        let platform = window.navigator.platform,
+            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+        
+        if (windowsPlatforms.indexOf(platform) !== -1){
+            document.body.classList.toggle('scale-80');
+        }
+
         let themeButton = document.getElementById('themeButton');
         let themeButtonMobile = document.getElementById('themeButtonMobile');
         let prismJS = document.getElementById('prism');
